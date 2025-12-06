edbe7899af8521520ea04b6ae8ed4841c553de10
Samuel Kyletoft <samuel@kyletoft.se>
2025-12-06 05:00:56 +0100

    Day 3: Both

--- dec03/Main.hs
+++ dec03/Main.hs
@@ -1,8 +1,44 @@
 {-# LANGUAGE GHC2024 #-}
 {-# OPTIONS_GHC -Wno-x-partial #-}
 
-part1 = const ""
+import Data.Set qualified as Set
 
-part2 = const ""
+newtype Pos = Pos (Int, Int) deriving (Eq, Ord, Show)
+
+Pos (x, y) +. Pos (z, w) = Pos (x + z, y + w)
+Pos (x, y) -. Pos (z, w) = Pos (x - z, y - w)
+
+move = \case
+  'v' -> Pos (0, -1)
+  '^' -> Pos (0, 1)
+  '<' -> Pos (-1, 0)
+  '>' -> Pos (1, 0)
+  '\n' -> Pos (0, 0)
+
+part1 =
+  show
+    . Set.size
+    . Set.fromList
+    . scanl (+.) (Pos (0, 0))
+    . map move
+
+takeOdd = \case
+  (x:_:xs) -> x:takeOdd xs
+  _ -> []
+
+takeEven = takeOdd . drop 1
+
+part2 input =
+  let santa =
+        scanl (+.) (Pos (0, 0))
+          . map move
+          . takeEven
+          $ input
+      robo =
+        scanl (+.) (Pos (0, 0))
+          . map move
+          . takeOdd
+          $ input
+   in show . Set.size . Set.fromList $ santa ++ robo
 
 main = interact (\s -> unlines [part1 s, part2 s])
