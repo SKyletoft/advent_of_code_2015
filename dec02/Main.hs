5a4b6b2f9173ed882ed1afc5927d50a2ad9d45b5
Samuel Kyletoft <samuel@kyletoft.se>
2025-12-06 04:48:09 +0100

    Day 2: Both

--- dec02/Main.hs
+++ dec02/Main.hs
@@ -1,8 +1,36 @@
 {-# LANGUAGE GHC2024 #-}
 {-# OPTIONS_GHC -Wno-x-partial #-}
 
-part1 = const ""
+import Data.List
+import Data.List.Split
 
-part2 = const ""
+part1 =
+  show
+    . sum
+    . map
+      ( ( \[l, w, h] ->
+            let x = l * w
+                y = w * h
+                z = h * l
+             in 2 * (x + y + z) + minimum [x, y, z]
+        )
+          . map (read :: String -> Int)
+          . splitOn "x"
+      )
+    . lines
+
+part2 =
+  show
+    . sum
+    . map
+      ( ( \sides ->
+          let around = (*2) . sum . take 2 . sort $ sides
+              bow = product sides
+           in around + bow
+        )
+          . map (read :: String -> Int)
+          . splitOn "x"
+      )
+    . lines
 
 main = interact (\s -> unlines [part1 s, part2 s])
